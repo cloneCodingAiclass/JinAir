31552cfb3fc19095c197cdeccb3888310bb7c431
yoo-youngeun <97489751+yoo-youngeun@users.noreply.github.com>
2022-02-27 13:19:02 +0900

    dtd

--- src/main/java/com/project/jinair/JinAirApplication.java
+++ src/main/java/com/project/jinair/JinAirApplication.java
@@ -9,6 +9,7 @@ public class JinAirApplication {
     public static void main(String[] args) {
         SpringApplication.run(JinAirApplication.class, args);
         System.out.println("dsfsfd");
+        System.out.println("sdfseuhdksjl;");
     }
 
 }
