4ae40972e78bdbfbd2d250203440207668e5ea01
Lee J <ljwwnd99@gmail.com>
2022-02-27 14:04:28 +0900

    Changes

--- src/main/java/com/project/jinair/controller/PageController.java
+++ src/main/java/com/project/jinair/controller/PageController.java
@@ -3,12 +3,15 @@ package com.project.jinair.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/pages")
 public class PageController {
-    @GetMapping("/admin_index")
-    public void adminIndex() {
 
+    @RequestMapping("/admin_index")
+    public ModelAndView adminIndex() {
+        return new ModelAndView("/adminpage/pages/admin_index")
+                .addObject("code", "admin_index");
     }
 }
