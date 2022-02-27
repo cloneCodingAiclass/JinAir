a61d166f6cfa0d2916405536fa183e333def6822
CHANYEONG YUN <92909812+YUNCHANYEONG@users.noreply.github.com>
2022-02-27 15:00:50 +0900

    고객관리

--- src/main/java/com/project/jinair/controller/PageController.java
+++ src/main/java/com/project/jinair/controller/PageController.java
@@ -14,4 +14,22 @@ public class PageController {
         return new ModelAndView("/adminpage/pages/admin_index")
                 .addObject("code", "admin_index");
     }
+
+    @RequestMapping("/admin/cm_list")
+    public ModelAndView cm_list() {
+        return new ModelAndView("/adminpage/pages/cm/cm_list")
+                .addObject("code", "cm_list");
+    }
+
+    @RequestMapping("/admin/cm_list/cm_detail")
+    public ModelAndView cm_detail() {
+        return new ModelAndView("/adminpage/pages/cm/cm_detail")
+                .addObject("code", "cm_list");
+    }
+
+    @RequestMapping("/admin/cm_list/cm_modify")
+    public ModelAndView cm_modify() {
+        return new ModelAndView("/adminpage/pages/cm/cm_modify")
+                .addObject("code", "cm_list");
+    }
 }
