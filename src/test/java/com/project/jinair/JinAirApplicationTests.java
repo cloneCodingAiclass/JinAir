caf873ae52ab5b80ea80e8c6c91f6348b598c9be
LaSeunghyun <rmwl2356@gmail.com>
2022-02-27 13:03:39 +0900

    가보자

---
+++ src/test/java/com/project/jinair/JinAirApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.jinair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JinAirApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
