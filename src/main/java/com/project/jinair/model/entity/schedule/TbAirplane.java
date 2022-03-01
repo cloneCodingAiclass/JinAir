8c5c043816029a380e4a8cb698ac393d0a9172b2
HWJB <cpzm012@gmail.com>
2022-03-01 20:02:14 +0900

    스케줄 entity

---
+++ src/main/java/com/project/jinair/model/entity/schedule/TbAirplane.java
@@ -0,0 +1,31 @@
+package com.project.jinair.model.entity.schedule;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@SequenceGenerator(
+        name ="seq_airplane_idx",
+        sequenceName = "seq_airplane_idx",
+        initialValue = 1,
+        allocationSize = 1
+)
+@EntityListeners(AuditingEntityListener.class)
+@Builder
+public class TbAirplane {   // 비행기 정보
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_airplane_idx")
+    private Long ap_index;
+    private String ap_name;
+    private String ap_id;
+    private Long ap_seat;
+}
