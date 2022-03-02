92404550b7bdbcc8c1dbc430fb67637e0e138730
HWJB <cpzm012@gmail.com>
2022-03-02 11:37:41 +0900

    이넘클래스 오타 수정

--- src/main/java/com/project/jinair/model/enumclass/AirplaneSeatStatus.java
+++ src/main/java/com/project/jinair/model/enumclass/AirplaneSeatStatus.java
@@ -11,9 +11,9 @@ public enum AirplaneSeatStatus {
     JiniPl(0,"지니 플러스", "Plus"),
     JiniSt(1,"지니 스트레치", "Stretch"),
     JiniFs(2,"지니 패스트", "Fast"),
-    JinuEE(3,"비상구열", "Emergency Exit"),
-    JinuSA(4,"스탠다드 A", "Standard A"),
-    JinuSB(5,"스탠다드 B", "Standard B");
+    JiniEE(3,"비상구열", "Emergency Exit"),
+    JiniSA(4,"스탠다드 A", "Standard A"),
+    JiniSB(5,"스탠다드 B", "Standard B");
 
 
     private Integer id;
