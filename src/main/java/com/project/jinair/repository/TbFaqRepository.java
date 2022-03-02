1d98d1ef8a193a503ac1cef1192a86e96735e5dd
CHANYEONG YUN <92909812+YUNCHANYEONG@users.noreply.github.com>
2022-03-02 14:32:33 +0900

    레퍼지토리 수정

--- src/main/java/com/project/jinair/repository/TbFaqRepository.java
+++ src/main/java/com/project/jinair/repository/TbFaqRepository.java
@@ -1,8 +1,9 @@
 package com.project.jinair.repository;
 
+import com.project.jinair.model.entity.board.TbFaq;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TbFaqRepository extends JpaRepository<TbFaqRepository, Long> {
+public interface TbFaqRepository extends JpaRepository<TbFaq, Long> {
 }
--- src/main/java/com/project/jinair/repository/TbMagazineRepository.java
+++ src/main/java/com/project/jinair/repository/TbMagazineRepository.java
@@ -1,8 +1,9 @@
 package com.project.jinair.repository;
 
+import com.project.jinair.model.entity.board.TbMagazine;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TbMagazineRepository extends JpaRepository<TbMagazineRepository, Long> {
+public interface TbMagazineRepository extends JpaRepository<TbMagazine, Long> {
 }
