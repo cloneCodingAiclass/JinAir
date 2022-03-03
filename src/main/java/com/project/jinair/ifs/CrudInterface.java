1605b75ef13629738e4a8bfed71478c4daffae3d
CHANYEONG YUN <92909812+YUNCHANYEONG@users.noreply.github.com>
2022-03-03 10:15:58 +0900

    헤더 푸시

--- src/main/java/com/project/jinair/ifs/CrudInterface.java
+++ src/main/java/com/project/jinair/ifs/CrudInterface.java
@@ -1,4 +1,11 @@
 package com.project.jinair.ifs;
 
-public interface CrudInterface {
+import com.project.jinair.model.network.Header;
+
+public interface CrudInterface <Req, Res>{
+
+    Header<Res> create(Header<Req> request);
+    Header<Res> read(Long id);
+    Header<Res> update(Header<Req> request);
+    Header<Res> delete(Long id);
 }
