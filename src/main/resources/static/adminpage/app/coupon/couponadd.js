e7277944f031eb847127bbc5b98bd589f2e3d7e9
LaSeunghyun <rmwl2356@gmail.com>
2022-03-08 22:13:21 +0900

    컨트롤러 서비스 푸쉬

--- src/main/resources/static/adminpage/app/coupon/couponadd.js
+++ src/main/resources/static/adminpage/app/coupon/couponadd.js
@@ -97,17 +97,39 @@ $(function () {
         $('.nav10').parent().siblings().find('li').css({"display":"none"});
         $('.nav10').siblings('li').eq(0).find('a').css({"color":"#BDD600"});
     })
+
+    $('#add_btn').on('click', function (){
+        let text = "";
+
+        let nation = $('#nation option:selected').text();
+        let route = $('#route option:selected').text();
+        let discount = $('#discount').val();
+        let startDate = $('#startDate').val();
+        let endDate = $('#endDate').val();
+        let totalcoupon = $('#totalcoupon').val();
+
+        text += '<p>' + nation + ' / ' + route + '<br> 할인율 : ' + discount + "%<br> 기간 : " + startDate + " ~ " + endDate + " <br> 총 " + totalcoupon + "매의 쿠폰을 발급하겠습니다." + '</p>'
+
+        if(totalcoupon == "" || discount == "" || startDate == "" || endDate == ""){
+            location.reload();
+        }else{
+            document.getElementById("modal_isfind_title").innerHTML = text;
+        }
+
+    });
 });
 
 $(()=> {
-    $('#modal_isfind').hide();
     $(".coupon_btn").on('click', () => {
+        $("#modal_isfind").css('display', 'flex');
         $("#modal_isfind").fadeIn();
     })
     $(".complete").on('click', () => {
+        $("#modal_isfind").css('display', 'none');
         $("#modal_isfind").fadeOut();
     })
     $(".uncomplete").on('click', () => {
+        $("#modal_isfind").css('display', 'none');
         $("#modal_isfind").fadeOut();
     })
 })
