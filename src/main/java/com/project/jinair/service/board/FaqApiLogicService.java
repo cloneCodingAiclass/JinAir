ac67f56899f24e509a1e32b6eb23dfe8bcd73d8e
HWJB <cpzm012@gmail.com>
2022-03-21 15:56:42 +0900

    faq 완료

--- src/main/java/com/project/jinair/service/board/FaqApiLogicService.java
+++ src/main/java/com/project/jinair/service/board/FaqApiLogicService.java
@@ -127,11 +127,18 @@ public class FaqApiLogicService implements CrudInterface<FaqApiRequest, FaqApiRe
         return Header.OK(faqApiResponseList);
     }
 
-    public Header<List<FaqApiResponse>> getFaqType(String a) {
-        List<TbFaq> tbFaq = tbFaqRepository.findByFaqTypeContaining(a);
+    public Header<List<FaqApiResponse>> getFaqType(String a, Pageable pageable) {
+        Page<TbFaq> tbFaq = tbFaqRepository.findByFaqTypeContaining(a, pageable);
         List<FaqApiResponse> faqApiResponseList = tbFaq.stream()
                 .map(user -> responseFaq(user))
                 .collect(Collectors.toList());
-        return Header.OK(faqApiResponseList);
+
+        Pagination pagination = Pagination.builder()
+                .totalPages(tbFaq.getTotalPages())
+                .totalElements(tbFaq.getTotalElements())
+                .currentPage(tbFaq.getNumber())
+                .currentElements(tbFaq.getNumberOfElements())
+                .build();
+        return Header.OK(faqApiResponseList, pagination);
     }
 }
