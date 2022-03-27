025645827e8acd58dbbee5db929828385cb4143d
LaSeunghyun <rmwl2356@gmail.com>
2022-03-27 11:13:15 +0900

    00

--- src/main/resources/static/Userpage/app/payment/extras.js
+++ src/main/resources/static/Userpage/app/payment/extras.js
@@ -1411,6 +1411,7 @@ $(function () {
       $("#option_price_list #option_ins").parent().remove();
         $("#option_price_list #option_ins_price").remove();
       }
+    $('body').css("overflow", "hidden");
   }
 
 
@@ -1923,6 +1924,7 @@ let isinsjoin = false;
     $(".img2").css("display", "none");
     $(".img").css("display", "block");
     $("#modal_totP_wrap").fadeOut();
+    $('body').css("overflow", "scroll");
     $(".fix_modal").fadeOut();
   })
   $(".img2").on('click', () => {
