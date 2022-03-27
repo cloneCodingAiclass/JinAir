6249e58059dc820d8e5e095616455b91e7bb9b14
HWJB <cpzm012@gmail.com>
2022-03-27 15:06:58 +0900

    퀵메뉴 # 삭제

--- src/main/resources/static/Userpage/app/index/quickmenu.js
+++ src/main/resources/static/Userpage/app/index/quickmenu.js
@@ -7,4 +7,10 @@ $(function () {
         let position = $(window).scrollTop() * 0.023; // 현재 스크롤바의 위치값 반환
         $('.quickmanu').stop().animate({ 'top': + position + currentPosition + 'px' }, 1000);
     });
+
+    $('.quicktop').click(function(){
+        $('html, body').animate({scrollTop:0},400);
+        return false;
+    });
+
 });
