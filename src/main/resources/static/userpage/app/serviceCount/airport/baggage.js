c02037e2794175bad732fcb1690b1113cfbf5709
yoo-youngeun <97489751+yoo-youngeun@users.noreply.github.com>
2022-03-27 15:41:29 +0900

    ...

--- src/main/resources/static/userpage/app/serviceCount/airport/baggage.js
+++ src/main/resources/static/userpage/app/serviceCount/airport/baggage.js
@@ -148,4 +148,232 @@ $(function () {
         $('.bag_compensation').css({"background-color":"white", "color": "#444"});
         $('#baggage3').css({"display":"none"});
     }
+
+
 });
+
+$(() => {
+    document.getElementById('fromDt').valueAsDate = new Date();
+    document.getElementById('toDt').valueAsDate = new Date();
+})
+
+$(() => {
+
+    let typeList = [
+        '필수 선택', '가방', '전자제품', '노트북/테블릿', '담배', '도서', '모자'
+        , '배게/담요', '시계', '식품', '신발', '안경/선글라스', '액세서리', '의류'
+        , '이어폰/휴대폰', '주류', '지갑', '화장품', '기타'
+    ]
+
+
+    let itemList = $("#itemType");
+    let arrAirport = $("#arvApo");
+    let airplane = $("#fltNo");
+
+    for (let i = 0; i < typeList.length; i++){
+        let option = document.createElement('option');
+        option.innerText = typeList[i];
+        option.value = typeList[i];
+        itemList.append(option);
+    }
+
+    airplaneType();
+    areaList();
+
+    // 항공기 타입 데이터 목록
+    function airplaneType(){
+        $.get("/api/airplane/list", function (response){
+            console.dir(response)
+            let arr = response.data.map(function (val, index){
+                return val['apType']
+            }).filter(function (val, index, arr2){
+                return arr2.indexOf(val) === index;
+            })
+
+            console.log(arr);
+
+            for (let i = 0; i < arr.length; i++){
+                let a = arr[i];
+                let option = document.createElement('option');
+                option.innerText = a;
+                option.value = a;
+                airplane.append(option);
+            }
+
+        })
+    }
+
+    // 출발지 도착지 데이터 목록
+    function areaList(){
+        $.get("/api/airport/list", function (response){
+            // 도착지 셀렉트
+            for (let i = 0; i < response.data.length; i++){
+                let a = response.data[i].aptAirport;
+                let option = document.createElement('option');
+                option.innerText = a;
+                option.value = a;
+                arrAirport.append(option);
+            }
+        })
+    }
+
+    let indexBtn = [];
+
+    let page;
+
+    let pagination = {
+        total_page : 0,
+        total_element : 0,
+        current_page : 0,
+        curren_elements : 0
+    }
+
+    // 페이지 정보
+    let showPage = new Vue({
+        el : '#showPage',
+        data : {
+            totalElements : {},
+            currentPage : {}
+        }
+    })
+
+    let lostList = new Vue({
+        el : '#lostList',
+        data : {
+            lostList : {}
+        }
+    })
+
+
+    list(0);
+
+    function list(page){
+        console.log("page : " + page);
+        $.get("/api/lost?page="+page, function (response){
+            indexBtn = [];
+            pagination = response.pagination;
+
+            showPage.totalElements = pagination.currentPage;
+            showPage.currentPage = pagination.currentPage;
+
+            lostList.lostList = response.data;
+
+            let lastPage = response.pagination.totalPages;
+
+            let str = "";
+
+            if (lastPage != 0) {
+                str += "<td class='firstPage1'><<</td>";
+            }
+            for (let i = 0; i < lastPage; i++) {
+                str += "<td class='pages' id="+i+">" + (i+1) + "</td>";
+            }
+            if (lastPage != 0){
+                str += "<td class='lastPage1'>>></td>";
+            }
+            $("#showPage").html(str);
+
+            if(page == 0) {
+                $(".firstPage1").css("visibility", "hidden");
+            }
+            if(page == lastPage-1) {
+                $(".lastPage1").css("visibility", "hidden");
+            }
+            $(".pages").css({
+                "background-color" : "#fff",
+                "color" : "#444",
+                "cursor" : "pointer"
+            });
+            $("#"+page+"").css({
+                "background-color" : "#661e43",
+                "color" : "white"
+            });
+            $("#showPage").on('click', '.firstPage1', function(){
+                list(0);
+            });
+            $("#showPage").on('click', '.lastPage1', function(){
+                list(lastPage-1);
+            });
+        })
+    }
+
+    let airplanee = "";
+    let airport = "";
+    let type = "";
+    let start = "";
+    let end = "";
+    $("#btnSearch").on('click', function (){
+        if($("#itemType").find('option:selected').val() == '필수 선택'){
+            alert('입력을 확인해주세요');
+        }else {
+            airplanee = $("#fltNo").find('option:selected').val();
+            airport = $("#arvApo").find('option:selected').val();
+            type = $("#itemType").find('option:selected').val();
+            start = $("#fromDt").val() + "T00:00:00";
+            end = $("#toDt").val() + "T00:00:00";
+            search(0, airplanee, airport, type, start, end);
+        }
+    })
+
+    function search(page, airplanee, airport, type, start, end){
+        $.post({
+            url: "/api/lost/search?page="+page,
+            data : "losAirplane=" + airplanee + "&losAirport=" + airport + "&losType=" + type + "&start=" + start + "&end=" + end,
+            dataType : 'text',
+            success : function(response){
+                console.dir(response)
+                let dataJson = JSON.parse(response)
+                console.dir(dataJson)
+                lostList.lostList = dataJson.data;
+
+                let lastPage = dataJson.pagination.totalPages;
+
+                let str2 = "";
+                if (lastPage != 0) {
+                    str2 += "<td class='firstPage2'><<</td>";
+                }
+                for (let i = 0; i < lastPage; i++) {
+                    str2 += "<td class='pagesS' id="+i+">" + (i+1) + "</td>";
+                }
+                if (lastPage != 0) {
+                    str2 += "<td class='lastPage2'>>></td>";
+                }
+                $("#showPage").html(str2);
+                if(page == 0) {
+                    $(".firstPage2").css("visibility", "hidden");
+                }
+                if(page == lastPage-1) {
+                    $(".lastPage2").css("visibility", "hidden");
+                }
+                $(".pagesS").css({
+                    "background-color" : "#fff",
+                    "color" : "#444",
+                    "cursor" : "pointer"
+                });
+                $("#"+page+"").css({
+                    "background-color" : "#661e43",
+                    "color" : "white"
+                });
+                $(document).on('click', '.firstPage2', function(){
+                    search(0, airplane, airport, type, start, end);
+                });
+                $(document).on('click', '.lastPage2', function(){
+                    search(lastPage, airplane, airport, type, start, end);
+                });
+            }
+        })
+    }
+
+
+    $("#showPage").on('click', '.pages', function(){
+        let pageId = this.id;
+        console.log(pageId);
+        list(pageId);
+    });
+
+    $("#showPage").on('click', '.pagesS', function(){
+        let pageId2 = this.id;
+        console.log(pageId2);
+        search(pageId2, airplane, airport, type, start, end);
+    });
+})
